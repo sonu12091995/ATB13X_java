432db2e1f1aeb4557342ccb3f7440eb20309b5af
sonu12091995 <sonu735700@gmail.com>
2025-06-06 12:51:13 +0530

    Hello world changes

--- src/ATB01/Laab001.java
+++ src/ATB01/Laab001.java
@@ -3,5 +3,10 @@ package ATB01;
 public class Laab001 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
     }
 }
