796cda5ca808802cc4cedf122b642015a59e3a28
sonu12091995 <sonu735700@gmail.com>
2025-06-06 12:53:41 +0530

    This is second push code

---
+++ src/ATB01/Lab002.java
@@ -0,0 +1,12 @@
+package ATB01;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Lab002 {
+    public static void main(String[] args) {
+
+        System.out.println("This is second push code");
+
+    }
+}
