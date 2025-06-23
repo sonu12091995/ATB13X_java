f1635bb1d0941922a60cb91674f513a3dffe53bd
sonu12091995 <sonu735700@gmail.com>
2025-06-23 13:00:20 +0200

    charAt_index

---
+++ src/String_Methods/charAt_index.java
@@ -0,0 +1,9 @@
+package String_Methods;
+
+public class charAt_index {
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        System.out.println(str.charAt(1)); // Output: 'e'
+    }
+}
