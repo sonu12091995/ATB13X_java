8f5f918ccedecb62bb14749162c40c0161472b7c
sonu12091995 <sonu735700@gmail.com>
2025-07-07 13:29:30 +0530

    over_loadinOveriding

--- src/oops_polymorphism/Overloading/Overrloadig_MathOpration.java
+++ src/oops_polymorphism/Overloading/Overrloadig_MathOpration.java
@@ -3,6 +3,9 @@ class MathOperations {
     // In the same class, When you have a method wit same
     // Same name methods but different arguments and different return type.
 
+    int add(){
+        return 0;
+    }
     int add(int a, int b) {
         return a + b;
     }
@@ -19,17 +22,17 @@ class MathOperations {
         return a + b;
     }
 }
-public class Overriding_MathOpration {
+public class Overrloadig_MathOpration {
     public static void main(String[] args) {
         MathOperations math  = new MathOperations();
-        int a = math.add(12,33);// firt method run
+     String a   = math.add("Sonu","Sneha");// firt method run
         System.out.println(a);
 
         int r2  = math.add(3,4,6);// Second method run
         System.out.println(r2);
 
         double r3  = math.add(3.14,4.56);
-        System.out.println(r3);// 3rd method run
+        System.out.println(r3);
     }
 
 }
